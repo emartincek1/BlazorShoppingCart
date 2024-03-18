fd5058b15a55b3a3bbc5d14fd4bb29c484a9e232
emartincek1 <evan.martincek@gmail.com>
2024-03-18 16:08:53 -0500

    create Dto conversion extention and product controller

--- ShopOnlineSolution/ShopOnline.Models/Dtos/ProductDto.cs
+++ ShopOnlineSolution/ShopOnline.Models/Dtos/ProductDto.cs
@@ -11,7 +11,7 @@ namespace ShopOnline.Models.Dtos
         public int Id { get; set; }
         public string Name { get; set; }
         public string Description { get; set; }
-        public string ImgURL { get; set; }
+        public string ImageURL { get; set; }
         public decimal Price { get; set; }
         public int Qty { get; set; }
         public int CategoryId { get; set; }
