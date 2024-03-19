e121504185facc72628d3554c6e2b2e5184eeb6c
emartincek1 <evan.martincek@gmail.com>
2024-03-19 15:26:28 -0500

    finish product repository. overload dto conversion, add get item to product controller, create get sigle item service

--- ShopOnlineSolution/ShopOnline.Api/Controllers/ProductController.cs
+++ ShopOnlineSolution/ShopOnline.Api/Controllers/ProductController.cs
@@ -45,5 +45,33 @@ namespace ShopOnline.Api.Controllers
                                 "Error retrieving data from the database");
             }
         }
+
+        [HttpGet("{id:int}")]
+        public async Task<ActionResult<ProductDto>> GetItem(int id)
+        {
+            try
+            {
+                var product = await productRepository.GetItem(id);
+               
+                if (product == null)
+                {
+                    return BadRequest();
+                }
+                else
+                {
+                    var productCategory = await productRepository.GetCategory(product.CategoryId);
+                    var productDto = product.ConvertToDto(productCategory);
+
+                    return Ok(productDto);
+                }
+
+
+            }
+            catch (Exception)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError,
+                                "Error retrieving data from the database");
+            }
+        }
     }
 }
---
+++ ShopOnlineSolution/ShopOnline.Web/Pages/ProductDetailsBase.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Components;
+using ShopOnline.Models.Dtos;
+using ShopOnline.Web.Services.Contracts;
+
+namespace ShopOnline.Web.Pages
+{
+    public class ProductDetailsBase : ComponentBase
+    {
+        [Parameter]
+        public int Id { get; set; }
+
+        [Inject]
+        public IProductService ProductService { get; set; }
+
+        public ProductDto Product { get; set; }
+        
+        public string ErrorMessage { get; set; }
+
+        protected override Task OnInitializedAsync()
+        {
+            return base.OnInitializedAsync();
+        }
+    }
+}
